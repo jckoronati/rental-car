0a64cd98956ed1e0f501ada7db6ec7e0e8f1b9e3
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-04-29 21:21:05 -0300

    update gitignore and create route import

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
 
 const categoriesRoute = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoute.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +16,12 @@ categoriesRoute.post('/', (request, response) => {
 
 categoriesRoute.get('/', (request, response) => {
   return listCategoryController.handle(request, response);
+});
+
+categoriesRoute.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+
+  return response.send();
 });
 
 export { categoriesRoute };
