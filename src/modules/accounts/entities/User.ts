bf4499ec18b0d77e1b36bd2bde1b975ed20996f1
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-02 20:01:07 -0300

    feat created user model

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
