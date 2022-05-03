7536b53af9217abf26a9d7043d856870196a27f7
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-03 19:05:22 -0300

    update users route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const userRoute = Router();
 
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 userRoute.post('/', createUserController.handle);
+userRoute.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle,
+);
 
 export { userRoute };
