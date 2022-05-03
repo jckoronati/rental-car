481daa035d97bf80f919d451f3b337d33de73815
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-03 07:46:59 -0300

    feat new method to find user by id

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -2,6 +2,7 @@ import { ICreateUserDTO } from '../dto/ICreateUserDTO';
 import { User } from '../entities/User';
 
 interface IUserRepository {
+  findById(id: string): Promise<User>;
   findByEmail(email: string): Promise<User>;
   create(data: ICreateUserDTO): Promise<void>;
 }
