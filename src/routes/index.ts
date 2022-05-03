3231890a3162add126a4bbde781873b3e1331617
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-03 06:10:24 -0300

    included index file for users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { categoriesRoute } from './categories.routes';
 import { specificationRoute } from './specification.routes';
+import { userRoute } from './users.routes';
 
 const router = Router();
 
 router.use('/categories', categoriesRoute);
 router.use('/specifications', specificationRoute);
+router.use('/users', userRoute);
 
 export { router };
