78f852c3ec096a79d5d7354026f40a2b32f2816b
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-03 08:37:59 -0300

    refactor errors at create user use case

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { ICreateUserDTO } from '../../dto/ICreateUserDTO';
 import { IUserRepository } from '../../repositories/IUserRepository';
 
@@ -19,7 +20,7 @@ class CreateUserUseCase {
   }: ICreateUserDTO): Promise<void> {
     const userAlreadyExists = await this.userRepository.findByEmail(email);
 
-    if (userAlreadyExists) throw new Error('User already exists');
+    if (userAlreadyExists) throw new AppError('User already exists');
 
     const passwordCrypt = await hash(password, 8);
 
