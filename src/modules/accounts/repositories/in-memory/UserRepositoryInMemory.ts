49010076e7d566f2f88370311c764f33a8d91368
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-04 19:14:41 -0300

    feat user repostiory in memory

---
+++ src/modules/accounts/repositories/in-memory/UserRepositoryInMemory.ts
@@ -0,0 +1,39 @@
+import { ICreateUserDTO } from '../../dto/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUserRepository } from '../IUserRepository';
+
+class UserRepositoryInMemory implements IUserRepository {
+  users: User[] = [];
+
+  async findById(id: string): Promise<User> {
+    const user = this.users.find(user => user.id === id);
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find(user => user.email === email);
+
+    return user;
+  }
+
+  async create({
+    driver_license,
+    email,
+    password,
+    name,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      email,
+      password,
+      name,
+    });
+
+    this.users.push(user);
+  }
+}
+
+export { UserRepositoryInMemory };
