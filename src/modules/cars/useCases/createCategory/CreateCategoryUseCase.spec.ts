bf5834b0686f9e2a50f5900dc00cbc29a1f39e60
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-04 07:44:03 -0300

    implemented tests to category object

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,57 @@
+import { AppError } from '../../../../errors/AppError';
+import { CategoryRepositoryInMemory } from '../../repositories/in-memory/CategoryRepositoryInMemory';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategory: CreateCategoryUseCase;
+let categoryRepositoryInMemory: CategoryRepositoryInMemory;
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+describe('Create a category', () => {
+  beforeEach(() => {
+    categoryRepositoryInMemory = new CategoryRepositoryInMemory();
+    createCategory = new CreateCategoryUseCase(categoryRepositoryInMemory);
+  });
+
+  it('should be able to create a new category', async () => {
+    const category: IRequest = {
+      name: 'Sedan',
+      description: 'Carro mais espaçoso com maior conforto',
+    };
+
+    const { name, description } = category;
+
+    await createCategory.execute({
+      name,
+      description,
+    });
+
+    const categoryCreated = await categoryRepositoryInMemory.findByName(name);
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category when name already in use', async () => {
+    expect(async () => {
+      const category: IRequest = {
+        name: 'Test name',
+        description: 'Test description',
+      };
+
+      const { name, description } = category;
+
+      await createCategory.execute({
+        name,
+        description,
+      });
+
+      await createCategory.execute({
+        name,
+        description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
