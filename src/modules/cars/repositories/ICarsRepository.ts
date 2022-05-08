05d45b64cee6afeade2ec5a14a37af12179179e6
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-08 09:23:50 -0300

    feat included findById at interface

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -3,6 +3,7 @@ import { Car } from '../infra/typeorm/entities/Car';
 
 interface ICarsRepository {
   create(data: ICreateCarsDTO): Promise<Car>;
+  findById(id: string): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
   findAllAvailable(
     brand?: string,
