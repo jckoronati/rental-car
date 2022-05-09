f0e3b4a394a5b505c3202a9ef1b4c2954993c2fd
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-09 20:16:31 -0300

    feat included new container for cars images

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,10 @@ import { container } from 'tsyringe';
 import { UserRepository } from '../../modules/accounts/infra/typeorm/repositories/UserRepository';
 import { IUserRepository } from '../../modules/accounts/repositories/IUserRepository';
 import { CarRepository } from '../../modules/cars/infra/typeorm/repository/CarRepository';
+import { CarsImagesRepository } from '../../modules/cars/infra/typeorm/repository/CarsImagesRepository';
 import { CategoryRepository } from '../../modules/cars/infra/typeorm/repository/CategoryRepository';
 import { SpecificationRepository } from '../../modules/cars/infra/typeorm/repository/SpecificationRepository';
+import { ICarsImagesRepository } from '../../modules/cars/repositories/ICarsImagesRepository';
 import { ICarsRepository } from '../../modules/cars/repositories/ICarsRepository';
 import { ICategoryRepository } from '../../modules/cars/repositories/ICategoryRepository';
 import { ISpecificationRepository } from '../../modules/cars/repositories/ISpecificationRepository';
@@ -22,3 +24,8 @@ container.registerSingleton<ISpecificationRepository>(
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 
 container.registerSingleton<ICarsRepository>('CarRepository', CarRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  'CarsImagesRepository',
+  CarsImagesRepository,
+);
