a3d8e684c08f894bf479243175f3f3d53aab87d5
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-10 20:21:13 -0300

    implemented rental repository

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,14 @@
+import { ICreateRentalDTO } from '../dto/ICreateRentalDTO';
+import { Rentals } from '../infra/typeorm/entities/Rentals';
+
+interface IRentalsRepository {
+  findOpenRentalByCar(car_id: string): Promise<Rentals>;
+  findOpenRentalByUser(user_id: string): Promise<Rentals>;
+  create({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rentals>;
+}
+
+export { IRentalsRepository };
