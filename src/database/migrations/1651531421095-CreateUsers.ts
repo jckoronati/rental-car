f7fa788ee18494940d6ffeb3e99486060decd7da
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-10 06:48:15 -0300

    fixed missing is primary declaration on user table

--- src/database/migrations/1651531421095-CreateUsers.ts
+++ src/database/migrations/1651531421095-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1651531421095 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
