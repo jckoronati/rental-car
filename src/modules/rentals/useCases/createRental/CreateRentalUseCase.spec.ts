01db98a9a62efc4e4dc8f2aaa565eaaab844dde7
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-10 20:23:26 -0300

    included rental car use case tests

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,56 @@
+import { AppError } from '../../../../shared/errors/AppError';
+import { RentalsRepositoryInMemory } from '../../repositories/in-memory/RentalsRepositoryInMemory';
+import { CreateRentalUseCase } from './CreateRentalUseCase';
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe('Create Rental', () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it('should be able to realize a car rental', async () => {
+    const rental = await createRentalUseCase.execute({
+      user_id: '12345',
+      car_id: '121212',
+      expected_return_date: new Date(),
+    });
+
+    expect(rental).toHaveProperty('id');
+    expect(rental).toHaveProperty('start_date');
+  });
+
+  it('should not be able to rent a car if user had a rental in progress', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: '12345',
+        car_id: '121212',
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: '12345',
+        car_id: '121212',
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to realize a rental when car is unavaible', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: '12345',
+        car_id: 'u90',
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: '54321',
+        car_id: 'u90',
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
