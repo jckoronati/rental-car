76d66dd233279c0b613eb1cd35bec7b56db014d3
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-11 07:14:44 -0300

    implemented rental repository

---
+++ src/modules/rentals/infra/typeorm/repositories/RentalRepository.ts
@@ -0,0 +1,44 @@
+import { Repository } from 'typeorm';
+
+import { AppDataSource } from '../../../../../../data-source';
+import { ICreateRentalDTO } from '../../../dto/ICreateRentalDTO';
+import { IRentalsRepository } from '../../../repositories/IRentalsRepository';
+import { Rentals } from '../entities/Rentals';
+
+class RentalsRepository implements IRentalsRepository {
+  private repository: Repository<Rentals>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(Rentals);
+  }
+
+  async findOpenRentalByCar(car_id: string): Promise<Rentals> {
+    const car = await this.repository.findOneBy({ car_id });
+
+    return car;
+  }
+
+  async findOpenRentalByUser(user_id: string): Promise<Rentals> {
+    const user = await this.repository.findOneBy({ user_id });
+
+    return user;
+  }
+
+  async create({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rentals> {
+    const rental = this.repository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    await this.repository.save(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepository };
