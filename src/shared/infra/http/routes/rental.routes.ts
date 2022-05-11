67835292907df49a42405906670a9bf1dbff6f5f
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-11 07:53:09 -0300

    include ensureAuthenticated on rental post route

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '../../../../modules/rentals/useCases/createRental/CreateRentalController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalRoute = Router();
 
 const createRentalController = new CreateRentalController();
 
-rentalRoute.post('/', createRentalController.handle);
+rentalRoute.post('/', ensureAuthenticated, createRentalController.handle);
 
 export { rentalRoute };
