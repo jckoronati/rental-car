47e373b1438a953920e93a7014550dbe726012f6
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-11 07:52:46 -0300

    implemented typeorm on rental entity

--- src/modules/rentals/infra/typeorm/entities/Rentals.ts
+++ src/modules/rentals/infra/typeorm/entities/Rentals.ts
@@ -1,22 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+@Entity('rentals')
 class Rentals {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
+  @Column()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
+  @Column()
   expected_return_date: Date;
 
+  @Column()
   amount: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
