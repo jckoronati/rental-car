f73a76a55fe144b6b53f688b3fe4b58355fa8584
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-12 21:45:23 -0300

    implemented updated available repository in memo

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -9,6 +9,12 @@ class CarsRepositoryInMemory implements ICarsRepository {
     return this.cars.find(car => car.id === id);
   }
 
+  async updateAvailable(id: string, available: boolean): Promise<void> {
+    const findIndex = this.cars.findIndex(car => car.id === id);
+
+    this.cars[findIndex].available = available;
+  }
+
   async findAllAvailable(
     brand?: string,
     category_id?: string,
