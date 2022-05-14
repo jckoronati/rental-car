4a744f9b72bbfa850ccbcd7118eefc06eb0164b4
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-14 18:46:40 -0300

    include declaration of relations on repo

--- src/modules/rentals/infra/typeorm/repositories/RentalRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalRepository.ts
@@ -15,6 +15,7 @@ class RentalsRepository implements IRentalsRepository {
   async findByUser(user_id: string): Promise<Rentals[]> {
     const rentals = await this.repository.find({
       where: { user_id },
+      relations: ['car'],
     });
 
     return rentals;
