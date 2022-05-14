60cdb44a17266f6c49a4d630781864a23487e85a
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-13 21:04:25 -0300

    append new route to rental object

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '../../../../modules/rentals/useCases/createRental/CreateRentalController';
+import { ReturnRentalController } from '../../../../modules/rentals/useCases/returnRental/ReturnRentalController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalRoute = Router();
 
 const createRentalController = new CreateRentalController();
+const returnRentalController = new ReturnRentalController();
 
 rentalRoute.post('/', ensureAuthenticated, createRentalController.handle);
+
+rentalRoute.post(
+  '/return/:id',
+  ensureAuthenticated,
+  returnRentalController.handle,
+);
 
 export { rentalRoute };
