7259bc34fd7d1f558c4df40ecfb46950293d5124
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-14 19:46:55 -0300

    implemented user tokens repo

---
+++ src/modules/accounts/infra/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,32 @@
+import { Repository } from 'typeorm';
+
+import { AppDataSource } from '../../../../../../data-source';
+import { IUserTokenDTO } from '../../../dto/IUserTokenDTO';
+import { IUserTokensRepository } from '../../../repositories/IUserTokensRepository';
+import { UserTokens } from '../entities/UserTokens';
+
+class UserTokensRepository implements IUserTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(UserTokens);
+  }
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: IUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create({
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export { UserTokensRepository };
