b0d10654ccfc74b8c596aad7088e76d8278d7b09
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-14 20:29:04 -0300

    append days to expire refresh token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,4 +1,7 @@
 export default {
   secretToken: '693b595d2a2d05a5775fefef93c7b455',
   expiresIn: '15m',
+  secretRefreshToken: 'dca89a85bb11f3adda0365ab38af3e5c',
+  expiresInRefreshToken: '30d',
+  expiresRefreshTokenDays: 30,
 };
