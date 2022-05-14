e2a924bc7212fb58cdde2ab60673fa3087774f1d
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-14 10:59:03 -0300

    implemented rentals route

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '../../../../modules/rentals/useCases/createRental/CreateRentalController';
+import { ListRentalsByUserController } from '../../../../modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 import { ReturnRentalController } from '../../../../modules/rentals/useCases/returnRental/ReturnRentalController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -8,6 +9,7 @@ const rentalRoute = Router();
 
 const createRentalController = new CreateRentalController();
 const returnRentalController = new ReturnRentalController();
+const listRentalByUserController = new ListRentalsByUserController();
 
 rentalRoute.post('/', ensureAuthenticated, createRentalController.handle);
 
@@ -15,6 +17,12 @@ rentalRoute.post(
   '/return/:id',
   ensureAuthenticated,
   returnRentalController.handle,
+);
+
+rentalRoute.get(
+  '/user',
+  ensureAuthenticated,
+  listRentalByUserController.handle,
 );
 
 export { rentalRoute };
