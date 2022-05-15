9272eca4990c30547b1778310547493ae61537fb
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-15 10:05:52 -0300

    append refresh token route at authenticate

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { AuthenticateUserController } from '../../../../modules/accounts/useCases/authenticateUser/AuthenticateUserController';
+import { RefreshTokenController } from '../../../../modules/accounts/useCases/refreshToken/RefreshTokenController';
 
 const authenticateRoute = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoute.post('/sessions', authenticateUserController.handle);
+authenticateRoute.post('/refresh-token', refreshTokenController.handle);
 
 export { authenticateRoute };
