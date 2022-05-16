5f763a1c374235de2b85a205256ab9f709948934
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-16 06:50:41 -0300

    created reset pass use case

---
+++ src/modules/accounts/useCases/resetPassword/ResetPasswordUseCase.ts
@@ -0,0 +1,22 @@
+import { inject } from 'tsyringe';
+
+import { AppError } from '../../../../shared/errors/AppError';
+import { IUserTokensRepository } from '../../repositories/IUserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordUseCase {
+  constructor(
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  async execute({ password, token }: IRequest) {
+    const userToken = '';
+
+    if (!userToken) throw new AppError('Invalid token!');
+  }
+}
